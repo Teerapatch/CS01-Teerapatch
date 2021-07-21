6748842d3b4f004ff5eb56e61ace8c817089a94b
Teerapatch <teerapatch.pee@gmail.com>
2021-07-21 11:26:03 +0700

    Update CS01-6.py

--- CS01-6.py
+++ CS01-6.py
@@ -1,5 +1,7 @@
 A = float(input("Enter your number : "))
-if (A < 10) :
-    print("YES!")
-elif (A > 10) :
-    print("NO!")
+if (A <= 10) :
+    print("Not Good")
+elif (A <= 20) :
+    print("Good")
+elif (A <= 30) :
+    print("Very Good")
