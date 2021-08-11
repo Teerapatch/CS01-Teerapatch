94692015f2e3954d334cc0003b687aa7c444f00c
Teerapatch <teerapatch.pee@gmail.com>
2021-08-11 11:58:17 +0700

    Update CS01-Max Min.py

--- CS01-Max Min.py
+++ CS01-Max Min.py
@@ -2,7 +2,7 @@ Geerob = int(input("Enter your Arrey loop : "))
 Awma = [ ]
 for i in range(Geerob):
     num = int(input("Enter your number : "))
-    Awma.append(int(num))
+    Awma += [num]
 print(Awma)
 print(min(Awma))
 print(max(Awma))
